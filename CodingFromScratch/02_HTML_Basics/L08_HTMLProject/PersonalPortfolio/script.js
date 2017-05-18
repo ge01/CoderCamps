f9bc4ad9f4b6feb90f29e9d028005bb0830ea4c0
ge01 <gabriel.escobedo01@utrgv.edu>
2017-05-18 09:34:44 -0500

    CFS JS - cleaned up portfolio.html and script.js

--- CodingFromScratch/02_HTML_Basics/L08_HTMLProject/PersonalPortfolio/script.js
+++ CodingFromScratch/02_HTML_Basics/L08_HTMLProject/PersonalPortfolio/script.js
@@ -1,60 +1,66 @@
 // The user should be able to click a button that reports on statistics about the text
 // The user should be able to insert or enter text into a text area
 document.getElementById("wordBtn").addEventListener("click", function(){
+  // Call the functions that will generate statistics about the text.
   totalWords();
   totalSentences();
-  console.log(totalWords());
-  console.log(totalSentences());
   avgWrdPerSent(totalWords(), totalSentences());
+
+  // Calculate the number of spaces
   var totalSpaces = totalWords() - 1;
-  console.log(totalSpaces);
-  document.getElementById("outSpaces").innerHTML = "Total Number of Spaces " + totalSpaces;  
+
+  // Display then number of spaces.
+  document.getElementById("outSpaces").innerHTML = "Total Number of Spaces " + totalSpaces;
 });
 
-/*
-document.getElementById("sentenceBtn").addEventListener("click", function(){
-  totalSentences();
-});
-*/
-// Total number of words
+// The totalWords function calculates the total number of words in the text area.
 function totalWords() {
+  // Get the string entered into the text area from portfolio.html.
   var words = document.getElementById('sentence').value;
-  var wordCounter = 1;
-  console.log(words[2]);
 
+  // Variable to hold the number of words counted.
+  // Set to 1 because the last word has not trailing space.
+  var wordCounter = 1;
+
+  // Count the words in the words string.
   for (var i = 0; i <= words.length; i++) {
     if(words[i] === " "){
       wordCounter++;
     }
-    console.log(words[i]);
   }
 
-  console.log(wordCounter);
+  // Display the number of words counted.
   document.getElementById('outWords').innerHTML = "Total Number of Words: " + wordCounter;
+
   return wordCounter;
 }
 
-// Total number of sentences
+// The totalSentences function calculates the total number of sentences.
 function totalSentences() {
+  // Get the string entered into the text area from portfolio.html.
   var textAreaString = document.getElementById("sentence").value;
+
+  // Variable to hold the number of periods counted.
   var periodCounter = 0;
 
-  console.log(textAreaString);
-
+  // Count the periods in textAreaString.
   for (var j = 0; j <= textAreaString.length; j++) {
     if (textAreaString[j] === ".") {
       periodCounter++;
     }
   }
-  console.log(periodCounter);
+
+  // Display the number of periods counted.
   document.getElementById("outSentences").innerHTML = "Total Number of Sentences: " + periodCounter;
+
   return periodCounter;
 }
 
-// Average words per sentence
+// The avgWrdPerSent function calculates the average words per sentence.
 function avgWrdPerSent(numWords, numSentences) {
+  // Calculate the average words per sentence.
   var wps = numWords / numSentences;
+
+  // Display the average words per sentence.
   document.getElementById("wordsPerSentence").innerHTML = "Average Words Per Sentence: " + wps;
 }
-
-// Total number of spaces
