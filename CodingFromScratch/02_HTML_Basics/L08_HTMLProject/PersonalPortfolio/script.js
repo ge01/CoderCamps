1efde404cfd4c1ea9ee2e571251e512c03ea1f0d
ge01 <gabriel.escobedo01@utrgv.edu>
2017-05-18 04:55:09 -0500

    experimenting with getElementById

--- CodingFromScratch/02_HTML_Basics/L08_HTMLProject/PersonalPortfolio/script.js
+++ CodingFromScratch/02_HTML_Basics/L08_HTMLProject/PersonalPortfolio/script.js
@@ -1,4 +1,9 @@
 // The user should be able to insert or enter text into a text area
+function myFunction(){
+  document.getElementById('demo').innerHTML = "Paragraph changed.";
+}
+
+
 // The user should be able to click a button that reports on statistics about the text
 // Total number of words
 // Total number of sentences
