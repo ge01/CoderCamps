e6d249ef3cc682bfeca464a2593e7fd6b4b60a87
ge01 <gabriel.escobedo01@utrgv.edu>
2017-05-18 03:47:24 -0500

    added script.js

---
+++ CodingFromScratch/02_HTML_Basics/L08_HTMLProject/PersonalPortfolio/script.js
@@ -0,0 +1,6 @@
+// The user should be able to insert or enter text into a text area
+// The user should be able to click a button that reports on statistics about the text
+// Total number of words
+// Total number of sentences
+// Average words per sentence
+// Total number of spaces
