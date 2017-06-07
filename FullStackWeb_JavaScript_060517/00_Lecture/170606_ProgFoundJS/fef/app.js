3704367ce7d40571efefeb7cca57ddebb0405826
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-06 20:56:18 -0500

    LEC - DOM manipulation

--- FullStackWeb_JavaScript_060517/00_Lecture/170606_ProgFoundJS/fef/app.js
+++ FullStackWeb_JavaScript_060517/00_Lecture/170606_ProgFoundJS/fef/app.js
@@ -1,16 +1,25 @@
 // Basic Data Types
 let myName = 'joe';
-console.log(myName);
+// console.log(myName);
 
 let myAge = 99;
-console.log(myAge);
+// console.log(myAge);
 
 let person = {
   myName1: 'isaac',
   myAge1: 99
 };
 
-console.log(person);
-console.log(person.myName1);
-console.log(person['myName1']);
-console.log(person['myAge1']);
+// console.log(person);
+// console.log(person.myName1);
+// console.log(person['myName1']);
+// console.log(person['myAge1']);
+
+// DOM
+// console.dir(document);
+
+// DOM manipulation
+let btn = document.getElementById('myBtn');
+btn.addEventListener('click', () => {
+  alert('hello world!');
+});
