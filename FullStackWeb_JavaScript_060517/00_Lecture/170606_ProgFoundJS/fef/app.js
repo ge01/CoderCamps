8c5b844f04d9b7acde79940338dc0b9d466daf15
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-06 20:40:19 -0500

    LEC - basic data types

--- FullStackWeb_JavaScript_060517/00_Lecture/170606_ProgFoundJS/fef/app.js
+++ FullStackWeb_JavaScript_060517/00_Lecture/170606_ProgFoundJS/fef/app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// Basic Data Types
+let myName = 'joe';
+console.log(myName);
+
+let myAge = 99;
+console.log(myAge);
+
+let person = {
+  myName1: 'isaac',
+  myAge1: 99
+};
+
+console.log(person);
+console.log(person.myName1);
+console.log(person['myName1']);
+console.log(person['myAge1']);
