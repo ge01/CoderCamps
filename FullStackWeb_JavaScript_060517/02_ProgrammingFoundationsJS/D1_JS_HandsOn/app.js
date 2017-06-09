61ea069f84a799a4f36341ae5714adbd619e9b19
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-08 21:05:00 -0500

    PFJS - input a number grade display a letter grade

--- FullStackWeb_JavaScript_060517/02_ProgrammingFoundationsJS/D1_JS_HandsOn/app.js
+++ FullStackWeb_JavaScript_060517/02_ProgrammingFoundationsJS/D1_JS_HandsOn/app.js
@@ -3,9 +3,26 @@ document.getElementById('myBtn').addEventListener("click", function(){
 });
 
 function computeGrade() {
-
+  // Get the grade from the DOM and assign it to the theGrade variable.
   var theGrade = document.getElementById('grade').value;
 
-  // Display Grade
-  document.getElementById('outGrade').innerHTML = "Your Grade: " + theGrade;
+  // Verify the number grade is between 0 and 100
+  while(theGrade < 0 || theGrade > 100) {
+    theGrade = prompt("Please enter a number between 0 an 100.");
+  }
+
+  // Determine the letter grade.
+  if(theGrade < 60)
+    letterGrade = "F";
+    else if(theGrade < 70)
+    letterGrade = "D";
+    else if(theGrade < 80)
+    letterGrade = "C";
+    else if(theGrade < 90)
+    letterGrade = "B";
+    else  // If we got this far, letterGrade must be >= 90
+    letterGrade = "A";
+
+  // Display the leeter Grade
+  document.getElementById('outGrade').innerHTML = "Your Grade: " + letterGrade;
 }
